d90e1ef6fc3e9b83c8777b29ec22419088a28dc3
deancohen1234 <dean3712@gmail.com>
2018-10-21 14:23:32 -0600

    Added safety net for dropped objects in VR space, and built

---
+++ Assets/Scripts/DroppedObjectArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroppedObjectArea : MonoBehaviour {
+
+    public Transform m_ReturnTransform;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if an object is pickupable and falls, send it back to workbench
+        if (other.gameObject.GetComponent<OVRGrabbable>())
+        {
+            other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            other.transform.position = m_ReturnTransform.position;
+        }
+    }
+}
